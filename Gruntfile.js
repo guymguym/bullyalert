f011f69a469b81d7343720d3c70e654aeaa82101
gu <guymguym@gmail.com>
2013-10-17 12:12:55 +0300

    Inital Bully Alert Web Server

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var fs = require('fs');
+
+module.exports = function(grunt) {
+
+	// Default tasks
+	grunt.registerTask('default', [
+		'bower',
+		'jshint'
+		// 'concat',
+		// 'uglify'
+	]);
+
+	grunt.registerTask('nodeunit', [
+		'nodeunit'
+	]);
+
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		bower: {
+			install: true
+		},
+		jshint: {
+			all: [
+				'**/*.js'
+			]
+		},
+	});
+
+	// Load the plugins
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+
+	// Define custom tasks
+	grunt.task.registerMultiTask('bower', 'Bower', function() {
+		// Force task into async mode and grab a handle to the "done" function.
+		var done = this.async();
+		grunt.util.spawn({
+			cmd: './node_modules/.bin/bower',
+			args: [this.target]
+		}, done);
+	});
+};
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node ./node_modules/.bin/supervisor web.js
